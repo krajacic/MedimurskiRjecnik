d5b70b877f1dbf77dd298ec26460efde12c7f499
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-04-13 09:55:48 +0200

    initial commit

--- app/src/main/java/cawabanga/com/medimurskirjecnik/DictionaryDatabase.java
+++ app/src/main/java/cawabanga/com/medimurskirjecnik/DictionaryDatabase.java
@@ -5,13 +5,15 @@ import android.content.Context;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 /**
- * Created by croatan on 18.3.2016. MedimurskiRjecnik.
+ * Created by croatan on 22.3.2016. MedimurskiRjecnik.
  */
 public class DictionaryDatabase extends SQLiteAssetHelper {
-    private static final String DATABASE_NAME = "rjecnik";
+
+    private static final String DATABASE_NAMES = "rjecnik";
     private static final int DATABASE_VERSION = 5;
 
-    public DictionaryDatabase(Context context){
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    public DictionaryDatabase(Context context) {
+        super(context, DATABASE_NAMES, null, DATABASE_VERSION);
     }
+
 }
--- app/src/main/java/cawabanga/com/medimurskirjecnik/MainActivity.java
+++ app/src/main/java/cawabanga/com/medimurskirjecnik/MainActivity.java
@@ -1,13 +1,64 @@
 package cawabanga.com.medimurskirjecnik;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
+/**
+ * Created by croatan on 22.3.2016. MedimurskiRjecnik.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    //private EditText filterText;
+    private ArrayAdapter<String> listAdapter;
+    private SearchView searchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //filterText = (EditText)findViewById(R.id.editText);
+        ListView itemList = (ListView)findViewById(R.id.listView);
+        searchText = (SearchView)findViewById(R.id.searchView);
+
+        DatabaseBackend databaseBackend = new DatabaseBackend(MainActivity.this);
+        String[] objects = databaseBackend.dictionaryWords();
+
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, objects);
+
+        itemList.setAdapter(listAdapter);
+        itemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // make Toast when click
+                //Toast.makeText(getApplicationContext(), "Position " + position, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, DictionaryActivity.class);
+                intent.putExtra("OBJECT_ID", position);
+                startActivity(intent);
+            }
+        });
+
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                listAdapter.getFilter().filter(query);
+                return false;
+            }
+        });
+
+
+
     }
 }
